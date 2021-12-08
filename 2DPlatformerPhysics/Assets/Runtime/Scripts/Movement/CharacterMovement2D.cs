7af9912864876840ec8f2c25e3175226b77f0539
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-12-07 20:52:23 -0800

    281 - Ground Detection

--- 2DPlatformerPhysics/Assets/Runtime/Scripts/Movement/CharacterMovement2D.cs
+++ 2DPlatformerPhysics/Assets/Runtime/Scripts/Movement/CharacterMovement2D.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
 public class CharacterMovement2D : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private float maxGroundSpeed = 10;
 
     [SerializeField] private float timeToMaxGroundSpeedSeconds = 0.2f;
+
+    [SerializeField] private LayerMask groundMask;
+
+    [Space]
+    [Header("Jump")]
     [SerializeField] private float jumpHeight = 100;
 
     [SerializeField] private float timeToJumpApex = 0.5f;
@@ -16,12 +22,19 @@ public class CharacterMovement2D : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    private Collider2D coll;
+
     private Vector2 velocity;
     private Vector2 input;
+
+    private bool isGrounded;
+
+    private Vector2 Position => transform.position;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
     }
 
     private void FixedUpdate()
@@ -31,7 +44,57 @@ public class CharacterMovement2D : MonoBehaviour
 
         velocity.y += Gravity * Time.fixedDeltaTime;
 
+        CheckVerticalCollision(ref velocity);
+
         rb.velocity = velocity;
+
+        Debug.DrawRay(Position + Vector2.up * coll.bounds.extents.y, velocity.normalized * 2);
+    }
+
+    private void CheckVerticalCollision(ref Vector2 velocity)
+    {
+        const float rayLength = 0.05f;
+
+        isGrounded = false;
+        if (velocity.y <= 0)//Check collision bottom        
+        {
+            {
+                var origin = Position + Vector2.down * rayLength * 0.5f;
+                var size = new Vector2(coll.bounds.extents.x, rayLength);
+
+                var hit = Physics2D.OverlapBox(
+                    origin,
+                    size,
+                    0,
+                    groundMask);
+
+                if (hit)
+                {
+                    isGrounded = true;
+                    velocity.y = 0;
+                }
+
+                DebugUtils.DrawBox(origin, size, hit ? Color.red : Color.white);
+            }
+        }
+        else//Check collision top
+        {
+            var origin = Position + Vector2.up * (coll.bounds.size.y + rayLength * 0.5f);
+            var size = new Vector2(coll.bounds.extents.x, rayLength);
+
+            var hit = Physics2D.OverlapBox(
+                origin,
+                size,
+                0,
+                groundMask);
+
+            if (hit)
+            {
+                velocity.y = 0;
+            }
+
+            DebugUtils.DrawBox(origin, size, hit ? Color.red : Color.white);
+        }
     }
 
     public void SetMoveInput(float horizontal)
@@ -41,12 +104,9 @@ public class CharacterMovement2D : MonoBehaviour
 
     public void Jump()
     {
-        velocity.y = JumpSpeed;
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(transform.position, velocity);
+        if (isGrounded)
+        {
+            velocity.y = JumpSpeed;
+        }
     }
 }
