5067e42854c13ebbb33aa0ef4b9f7d97ad6eb26f
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-01-30 07:02:24 -0800

    293 - Desafio: Implementar Bomba

--- PhysicsSandbox/Assets/Runtime/Scripts/PlayerController.cs
+++ PhysicsSandbox/Assets/Runtime/Scripts/PlayerController.cs
@@ -2,22 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AirPlaneMovement))]
+[RequireComponent(typeof(AirPlaneMovement), typeof(BombLauncher))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float steeringSmoothSpeed = 1.5f;
 
     private AirPlaneMovement airPlaneMovement;
+    private BombLauncher projectileLauncher;
 
     private void Awake()
     {
         airPlaneMovement = GetComponent<AirPlaneMovement>();
+        projectileLauncher = GetComponent<BombLauncher>();
     }
 
     private void Update()
     {
         var frameInput = new Vector2(Input.GetAxisRaw("Vertical"), Input.GetAxisRaw("Horizontal"));
         airPlaneMovement.SetSteerInput(frameInput);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            projectileLauncher.TryShoot();
+        }
     }
 }
