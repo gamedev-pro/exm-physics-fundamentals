b236c25edf065441c56ac7b1be05352bdf44d159
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-02-08 05:32:20 -0800

    292 - Relembrando collision events

---
+++ PhysicsSandbox/Assets/Runtime/Scripts/Wall.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    [SerializeField] private float repelForce = 1;
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.TryGetComponent<AirPlaneMovement>(out var airPlaneMovement) &&
+            other.gameObject.TryGetComponent<Rigidbody>(out var rb))
+        {
+            for (int i = 0; i < other.contactCount; i++)
+            {
+                var contact = other.GetContact(i);
+                rb.AddForceAtPosition(-contact.normal * repelForce, contact.point);
+            }
+        }
+    }
+}
